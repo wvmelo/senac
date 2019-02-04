ca4bb861562c86719afbf2fcbca79b2f240a36b1
wvmelo <wagner.vianademelo@gmail.com>
2019-02-04 10:00:21 -0200

    Inclusão de projeto

---
+++ Test/Assets/Scripts/movimentoEsfera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoEsfera : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moviment = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(moviment * speed);
+	}
+
+    void Update() {
+
+    }
+
+}
